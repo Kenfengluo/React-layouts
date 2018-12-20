1118e60e53e3b884e903b8723ec409e681b3e671
ken luo <kenluofeng@gmail.com>
2018-12-19 22:28:37 -0500

    checked in files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'typeface-roboto';
+import SignIn from './signIn/signIn';
+import Dashboard from './dashboard/Dashboard';
+import Checkout from './checkOut/Checkout';
+
+const App = () => (
+  <Checkout />
+);
+
+export default App;
