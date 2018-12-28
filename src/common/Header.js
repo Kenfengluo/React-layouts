987893274fe37966315daf8ad3fff601bad103ea
Feng Luo <q13417@cusa.canon.com>
2018-12-28 11:58:06 -0500

    add courses page

--- src/common/Header.js
+++ src/common/Header.js
@@ -7,6 +7,8 @@ const Header = () => {
         <nav>
             <IndexLink to="/home" activeClassName="active">Home</IndexLink>
             { " | "}
+            <IndexLink to="/courses" activeClassName="active">Courses</IndexLink>
+            { " | "}
             <IndexLink to="/about" activeClassName="active">About</IndexLink>
         </nav>
     )
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route } from 'react-router';
 import App from './App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import CoursesPage from './components/course/CoursesPage';
 
 export default (
     <Route path="/" component={App}>
         <Route path ="/home" component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
         <Route path="/about" component={AboutPage} />
     </Route>
 );
